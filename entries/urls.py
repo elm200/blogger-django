5c2e8b0eb920ee8cbc303e9c5405c69c992e8d2c
Eiji Sakai <eijisakai@gmail.com>
2017-08-26 18:26:41 +0900

    エントリー編集画面

--- entries/urls.py
+++ entries/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<entry_id>[0-9]+)$', views.show, name='show'),
+    url(r'^(?P<entry_id>[0-9]+)/edit$', views.edit, name='edit'),
+    url(r'^(?P<entry_id>[0-9]+)/update$', views.update, name='update'),
 ]
--- entries/views.py
+++ entries/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 from .models import Entry
 
 def index(request):
@@ -10,3 +13,17 @@ def show(request, entry_id):
     entry = get_object_or_404(Entry, pk=entry_id)
     context = { 'entry': entry, 'title': 'エントリー詳細' }
     return render(request, 'entries/show.html', context)
+
+def edit(request, entry_id):
+    entry = get_object_or_404(Entry, pk=entry_id)
+    context = { 'entry': entry, 'title': 'エントリー編集' }
+    return render(request, 'entries/edit.html', context)
+
+def update(request, entry_id):
+    entry = get_object_or_404(Entry, pk=entry_id)
+
+    entry.title = request.POST['title']
+    entry.body = request.POST['body']
+    entry.save()
+
+    return HttpResponseRedirect(reverse("show", args=(entry.id,)))
